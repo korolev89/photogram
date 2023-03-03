86a51deb5c5bdeaf86d60de370d59214e7279bde
Anton Korolev <anton.korolev@arctic7.com>
2023-03-03 18:38:10 +0100

    main page implemented

--- app/app.py
+++ app/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from blueprints.main.views import main_blueprint
+from blueprints.posts.views import post_blueprint
 
 app = Flask(__name__, template_folder="/templates")
 
 app.register_blueprint(main_blueprint)
+app.register_blueprint(post_blueprint, url_prefix="/posts/")
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
--- app/blueprints/main/views.py
+++ app/blueprints/main/views.py
@@ -1,9 +1,8 @@
-from flask import Blueprint
-from flask import render_template, request
+from flask import Blueprint, redirect
 
 main_blueprint = Blueprint("main_blueprint", __name__, template_folder="templates")
 
 
 @main_blueprint.route("/")
 def main_page():
-    return render_template("index.html")
+    return redirect("/posts/", 302)
